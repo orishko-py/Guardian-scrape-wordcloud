922a526d710226aa31dc7206044ea35edaa8c7ab
orishko-py <52841414+orishko-py@users.noreply.github.com>
2019-07-28 14:19:37 +0100

    Add files via upload
    the files one needs to modify for an easy scrapy project are items and the putting all the crawling documents into spiders folder.
    I wrote script.sh to crawl through all ten topics with one command and save all info into separate csv files. 
    An example of the final output is in politics_example.png

---
+++ website_1/script.sh
@@ -0,0 +1,10 @@
+scrapy crawl sport_scraper -o sport.csv
+scrapy crawl gb_scraper -o gb.csv
+scrapy crawl politics_scraper -o politics.csv
+scrapy crawl cities_scraper -o cities.csv
+scrapy crawl culture_scraper -o culture.csv
+scrapy crawl science_scraper -o science.csv
+scrapy crawl society_scraper -o society.csv
+scrapy crawl environment_scraper -o environment.csv
+scrapy crawl technology_scraper -o technology.csv
+scrapy crawl law_scraper -o law.csv
---
+++ website_1/spiders/guardian_sport_scrape.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import scrapy
+from website_1.items import Website1Item
+import re
+import datetime 
+
+now = datetime.datetime.now()
+
+num = ['1','2','3','4','5','6','7','8','9','10','11','12']
+let = ['jan','feb','mar','apr','may','jun','jul','aug','sep','oct','nov','dec']
+month_index = num.index(str(now.month))
+month = let[month_index]
+
+today = str(now.year)+'/'+month+'/'+str(now.day)
+
+class Website1(scrapy.Spider):
+
+    name = "sport_scraper"
+
+    # First Start Url
+
+    guardian_url = ['https://www.theguardian.com/']
+    start_urls = []
+    topics = ['technology', 'environment', 'science', 'sport', 'law', 'politics', 'society', 'global-development', 'cities', 'culture']
+
+    start_urls.append('https://www.theguardian.com/'+'sport'+'/'+today)
+
+    def parse(self, response):
+
+        for href in response.xpath("//a[contains(@class, 'u-faux-block-link__overlay js-headline-text')]//@href"):
+
+            url  = href.extract() 
+
+            yield scrapy.Request(url, callback=self.parse_dir_contents)
+
+    def parse_dir_contents(self, response):
+
+        item = Website1Item()
+
+        # Getting Article Title
+
+        item['title'] = response.xpath("//title/descendant::text()").extract()[0].strip()
+
+        # Getting Article Description
+
+        item['description']= response.xpath("//meta[contains(@name, 'description')]//@content").extract()[0]
+
+        # First Published
+
+        item['date'] = response.xpath("//time[contains(@itemprop, 'datePublished')]/@datetime").extract()[0][:10]
+
+        # Url (The link to the page)
+
+        item['url'] = response.xpath("//meta[@property='og:url']/@content").extract()
+
+        yield item
+
+# In terminal use scrapy crawl sport_scraper -o Sport.csv
+
